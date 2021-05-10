29f16ec6846e62327c8808bd2fd3f8550f9e9c18
Vitor Hugo <vitor.h8@icloud.com>
2021-05-10 19:59:33 -0300

    Initial Commit

---
+++ Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by Vitor Hugo on 10/05/21.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
